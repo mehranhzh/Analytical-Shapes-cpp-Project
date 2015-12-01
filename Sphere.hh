b73756b06196165d996ff8ffeb0d3a29dc950a03
Hongshen2010 <yhongshe@stevens.edu>
2015-11-30 21:34:03 -0500

    Revert "Add triangulation of sphere"
    This reverts commit 2ee0d47a2c8d8c09a6b30526824be223a3d7b7f9.

--- Sphere.hh
+++ Sphere.hh
@@ -1,10 +1,6 @@
 #ifndef SPHERE_HH_
 #define SPHERE_HH_
 
-#include "Point.hh"
-#include <cmath>
-#include <vector>
-
 class Sphere{
 private:
     Point center_;
@@ -22,29 +18,6 @@ public:
 
     void set_center(const Point& c){ center_ = c;}
     void set_radius(double r) {radius_ = r;}
-
-    std::vector<Point> Points;
-
-    void Triangulate(center(), radius()) const {
-        double a = center().x();
-        double b = center().y();
-        double c = center().z();
-        double angle = PI / 180 * 10;
-        int p2 = 360 / 20;
-        int r2 = 180 / 20;
-        for(int y = -r2; y < r2; y++){
-            double cy = cos(y * angle);
-            double cy1 = cos((y + 1) * angle);
-            double sy = sin(y * angle);
-            double sy1 = sin((y + 1) * angle);
-            for(int i = -p2; i < p2; i++){
-                double ci = cos(i * angle);
-                double si = sin(i * angle);
-                Points.push_back(Point(a+r * ci*cy, b+r*sy, c+r * si*cy));
-                Points.push_back(Point(a+r * ci*cy1, b+r*sy1, c+r * si*cy1));
-            }
-        }
-    }
 
 };
 
