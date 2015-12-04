38645e39aebb228d632fb8006a93334bc1a8d125
Hongshen <yhongshe@stevens.edu>
2015-12-04 15:11:03 -0500

    Update Cube.hh

--- Cube.hh
+++ Cube.hh
@@ -1,16 +1,30 @@
+//Author: Yu Hongshen
+//cubic class
+
 #ifndef CUBE_HH_
 #define CUBE_HH_
 
-#include <fstream>
-#include <iostream>
 using namespace std;
 
 class Cube{
 private:
-    double a, double b, double c;
+    double a_, b_, c_;   //the length of every side
 public:
-    Cube(double a = 1, double b = 1, double c = 1);
+    Cube(double a_ = 1, double b_ = 1, double c_ = 1);
 
+    //accessors
+    double a() const {return a_;}
+    double b() const {return b_;}
+    double c() const {return c_;}
+
+    //set some values
+    void set_a(double a){a_ = a;}
+    void set_b(double b){b_ = b;}
+    void set_c(double c){c_ = c;}
+
+    double getVolume(){
+        return a()*b()*c();
+    }
 };
 
 #endif // CUBE_HH_
