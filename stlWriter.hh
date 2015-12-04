e6f3f0f45a57657a5f4eec39f46cfa0494028d79
Hongshen <yhongshe@stevens.edu>
2015-12-04 15:08:03 -0500

    Update stlWriter.hh

--- stlWriter.hh
+++ stlWriter.hh
@@ -1,7 +1,16 @@
+//Author: Yu Hongshen
+//a class that writes .stl files,
+//STL files need information of coordinates of triangle vertices and its normal.
+
 #ifndef STLWRITER_HH_
 #define STLWRITER_HH_
 
-class stlWriter(){
+//#include "TriangleMesh.hh"
+
+#include <iostream>
+#include <fstream>
+
+class stlWriter{
 
 };
 #endif // STLWRITER_HH_
