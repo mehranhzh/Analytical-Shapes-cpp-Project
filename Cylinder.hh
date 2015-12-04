d18c7b7a416f29b0f6f6fdf35e3b2a3c175939b3
Hongshen <yhongshe@stevens.edu>
2015-12-04 15:10:48 -0500

    Update Cylinder.hh

--- Cylinder.hh
+++ Cylinder.hh
@@ -1,17 +1,47 @@
+//Author: Yu Hongshen
+//cylinder class
+
 #ifndef CYLINDER_HH_
 #define CYLINDER_HH_
 
 #include <cmath>
 #include <fstream>
-#include <iostream>
 #include "point.hh"
 using namespace std;
 
+extern double PI;
+
 class Cylinder{
 private:
-    double h, r1, r2;
+    double h_, r1_, r2_;   //the height and two radii.
+    Point bottomCenter;    //we use the bottom center to position a cylinder
 public:
-    Cylinder(double h = 1, double r1 = 1, double r2 = 1);
+    Cylinder():
+        h_(1), r1_(1), r2_(1), bottomCenter(Point(0,0,0)){}
+    Cylinder(double h, double r1, double r2, Point bp):
+        h_(h), r1_(r1), r2_(r2), bottomCenter(bp){}
+
+    //accessors
+    Point center() const {return bottomCenter;}
+    double r1() const {return r1_;}
+    double r2() const {return r2_;}
+    double h() const {return h_;}
+
+    //set some values
+    void set_center(const Point& c){ bottomCenter = c;}
+    void set_r1(double r) {r1_ = r;}
+    void set_r2(double r) {r2_ = r;}
+    void set_h(double h) {h_ = h;}
+    void set_bottomCenter(const Point& bp) {bottomCenter = bp;}
+
+
+    double getVolume(){
+        if(r1() > r2()){
+            return h()*PI*((r1()-r2())*(r1()-r2())+r2()*r2());
+        }
+        return h()*PI*((r2()-r1()*(r2()-r1())+r1()*r1()));
+    }
 
 };
 #endif // CYLINDER_HH_1
+
