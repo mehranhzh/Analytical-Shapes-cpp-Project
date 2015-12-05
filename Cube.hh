a31891cd1a5be0f7a6beaf80a902f991f34624f9
Hongshen <yhongshe@stevens.edu>
2015-12-04 23:16:47 -0500

    Update Cube.hh

--- Cube.hh
+++ Cube.hh
@@ -4,26 +4,40 @@
 #ifndef CUBE_HH_
 #define CUBE_HH_
 
+#include <vector>
+
 using namespace std;
 
 class Cube{
 private:
-    double a_, b_, c_;   //the length of every side
+    Point Position_;
+    double a_, b_, c_; //the length of every side
 public:
-    Cube(double a_ = 1, double b_ = 1, double c_ = 1);
+    Cube(Point p = Point(0,0,0), double a = 1, double b = 1, double c = 1):
+        Position_(p), a_(a), b_(b), c_(c){}
 
     //accessors
     double a() const {return a_;}
     double b() const {return b_;}
     double c() const {return c_;}
+    Point Position() const {return Position_;}
 
     //set some values
     void set_a(double a){a_ = a;}
     void set_b(double b){b_ = b;}
     void set_c(double c){c_ = c;}
+    void set_Position(const Point& point) {Position_ = point;}
+
+    std::vector<Point> Points;
+    void Triangulate(){
+
+    }
 
     double getVolume(){
         return a()*b()*c();
+    }
+    double getArea(){
+        return 2*a()*b() + 2*a()*c() + 2*b()*c();
     }
 };
 
