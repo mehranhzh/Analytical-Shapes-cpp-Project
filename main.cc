6065e86daf370c524a9912d87379967b9acaf6ca
Hongshen <yhongshe@stevens.edu>
2015-12-05 21:54:45 -0500

    Update main.cc

--- main.cc
+++ main.cc
@@ -1,4 +1,5 @@
 #include "Point.hh"
+//#include "Point2D.hh"
 #include "Cube.hh"
 #include "Cylinder.hh"
 #include "Sphere.hh"
@@ -7,8 +8,15 @@
 #include "Triangle.hh"
 #include <iostream>
 
-double PI = 3.14159265;
+double PI = 3.14159265358979323846;
 
 int main(int argc, char** argv){
 
+    Cylinder c(10, 5, 5, Point(0,0,0));
+
+    c.getSTLfile();
+    //Triangle test(Point(1, 0, 0), Point(0, 0, 0), Point(0, 1, 0));
+    //std::cout << test.Normal();
+
+    return 0;
 }
