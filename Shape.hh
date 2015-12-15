9e5a9d4b717eefc214ef22d0a4994601c26c02ef
Mehran <EqSan@users.noreply.github.com>
2015-12-15 02:26:04 -0500

    Update Shape.hh
    Shape based class of 2D&3D Shape class

--- Shape.hh
+++ Shape.hh
@@ -1,22 +1,23 @@
+//Author:Mehran
 //Shape class will based class of other shapes
 #ifndef SHAPE_HH_
 #define SHAPE_HH_
 
-class Shape{//abstract class shape
-private:
-    double x, y, z;
+#include <iostream>
+#include "Point.hh"
+
+
+class Shape {//abstract class shape
 public:
-    Shape(double x1, double y1, double z1);//Constructor 3D
-    Shape(double x1, double y1);//Constructor 2D
-    double getX(), getY(), getZ();//get position x,y,z
-    void setX(double x1);//set position x
-    void setY(double y1);//set position y
-    void setZ(double z1);//set position z
+    Shape(double x1, double y1, double z1);//Constructor Shape
     virtual void moveTo(double x1, double y1, double z1)=0;//pure virtual function that move position to new position
-    //virtual bool isInside(double x, double y, double z) = 0; //check if shape is inside of another one
+    virtual bool isInside(double x1, double y1, double z1) = 0; //check if point is inside of Shape or not
     virtual double area() const = 0;//calculate the area of shapes
+    virtual double perimeter() const = 0;//calculate the perimeter of shapes
     virtual void draw()=0;//draw graphs or shapes
     virtual void volume()=0;//draw graphs or shapes
+    virtual void printShapeName() const = 0;
+    virtual void print() const = 0;
     virtual ~Shape(){};//Destructor
 
 };
