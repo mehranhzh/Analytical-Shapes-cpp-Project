ab54d051fa000fc13543db36357290633739e8c8
Mehran <EqSan@users.noreply.github.com>
2015-12-15 02:24:33 -0500

    Update Rectangle.hh
    Updated to get and set width&Height and print out rectangle area

--- Rectangle.hh
+++ Rectangle.hh
@@ -1,17 +1,22 @@
 #ifndef RECTANGLE_HH_
 #define RECTANGLE_HH_
 
+#include "Point.hh"
 
-class Rectangle: public Shape{
+class Rectangle: public Point{
 private:
-    double w, double h;
+    double width, height;
 public:
-    Rectangle(double x1, double y1, double z1, double w1, double h1);
-    double getW();
-    double getH();
-    void setW(){ double w1; }
-    void setH(){ double h1; }
-    void draw();
+    Rectangle(double x, double y, double w, double h) : Point(x, y, 0) { width = w; height= h; };
+    double getW() const { return width; };
+    double getH() const { return height; };
+    void setW(double w){ width = w; };
+    void setH(double h){ height = h; };
+    double area() const { return width*height; }
+    void printShapeName() const { cout << "Rectangle: "; }
+    friend ostream& operator <<(ostream& os, const Rectangle& r){
+        return os << "Center= " << "(" << r.x()<< "," << r.y() << "); Width= " << r.width << " Height= " << r.height; // Center= (x,y); Radius=
+    }
 
 };
 
