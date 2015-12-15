fe82b79bef7916ca4961a7d2251e07c0e91b5ec1
Mehran <EqSan@users.noreply.github.com>
2015-12-15 02:36:21 -0500

    Create Cylinder2.hh
    Shape3D Cylinder2.hh is simpler than Cylinder.hh just used Circle class

---
+++ Cylinder2.hh
@@ -0,0 +1,25 @@
+#ifndef Cylinder_HH_
+#define Cylinder_HH_
+
+#include "Circle.hh"
+
+class Cylinder : public Circle{
+private:
+    double height, radius;
+public:
+    Cylinder(double x, double y, double r, double h) : Circle(x, y, r) { height = h > 0 ? h : 0; };
+    //double getR() const { return radius; };
+    void setH(double h){ height = h > 0 ? h : 0; }
+    double area() const { return 2*Circle::area()+2*3.14*Circle::getR()*height; }
+    double volume() const { return Circle::area()*height; }
+    void printShapeName() const { cout << "Cylinder: "; }
+    friend ostream& operator <<(ostream& os, const Cylinder& c){
+        return os << "Center= " << "(" << c.x()<< "," << c.y() << "); Height= "
+        << c.height << "; Radius= " << c.Circle::getR(); ; // Center= (x,y); Height= ;Radius=
+    }
+
+
+
+};
+
+#endif // Cylinder_H
