fafb2f9bb68d89f627578b6a5c9c1e0e27b66bd9
Mehran <EqSan@users.noreply.github.com>
2015-12-15 02:21:33 -0500

    Update Circle.hh
    Updated to get and set radius and print circle area

--- Circle.hh
+++ Circle.hh
@@ -1,14 +1,21 @@
 #ifndef CIRCLE_HH_
 #define CIRCLE_HH_
 
-class Circle : public Shape{
+#include "Point.hh"
+
+class Circle : public Point{
 private:
-    double r;
+    double radius;
 public:
-    Circle(double x1, double y1, double z1, double r1);
-    double getR();
-    void setR(){double r1};
-    void draw();
+    Circle(double x, double y, double r) : Point(x, y, 0) { radius = r; };
+    double getR() const { return radius; };
+    void setR(double r){ radius = r; };
+    double area() const { return PI*radius*radius; }
+    void printShapeName() const { cout << "Circle: "; }
+    friend ostream& operator <<(ostream& os, const Circle& c){
+        return os << "Center= " << "(" << c.x()<< "," << c.y() << "); Radius= " << c.radius; // Center= (x,y); Radius=
+    }
+
 
 
 };
