e66a116ed0edc0648a1190aff49fd0bde3a9dffb
Hongshen <yhongshe@stevens.edu>
2015-12-17 11:18:08 -0500

    Got improved cube

--- Cube.hh
+++ Cube.hh
@@ -5,6 +5,9 @@
 #define CUBE_HH_
 
 #include <vector>
+#include <fstream>
+#include "Point.hh"
+#include "Triangle.hh"
 
 using namespace std;
 
@@ -20,7 +23,7 @@ public:
     double a() const {return a_;}
     double b() const {return b_;}
     double c() const {return c_;}
-    Point Position() const {return Position_;}
+    const Point& Position() const {return Position_;}
 
     //set some values
     void set_a(double a){a_ = a;}
@@ -29,8 +32,47 @@ public:
     void set_Position(const Point& point) {Position_ = point;}
 
     std::vector<Point> Points;
-    void Triangulate(){
+    std::vector<Triangle> Triangles;
 
+    void getSTLfile(){
+        Points.push_back(Point(a()+Position().x(), Position().y(), Position().z()));
+        Points.push_back(Point(a()+Position().x(), b()+Position().y(), Position().z()));
+        Points.push_back(Point(Position().x(), b()+Position().y(), Position().z()));
+        Points.push_back(Point(Position().x(), Position().y(), Position().z()));
+        Points.push_back(Point(a()+Position().x(), Position().y(), c()+Position().z()));
+        Points.push_back(Point(a()+Position().x(), b()+Position().y(), c()+Position().z()));
+        Points.push_back(Point(Position().x(), b()+Position().y(), c()+Position().z()));
+        Points.push_back(Point(Position().x(), Position().y(), c()+Position().z()));
+
+        Triangles.push_back(Triangle(Points[0], Points[1], Points[2]));
+        Triangles.push_back(Triangle(Points[0], Points[2], Points[3]));
+        Triangles.push_back(Triangle(Points[1], Points[4], Points[0]));
+        Triangles.push_back(Triangle(Points[1], Points[5], Points[4]));
+        Triangles.push_back(Triangle(Points[2], Points[5], Points[1]));
+        Triangles.push_back(Triangle(Points[2], Points[6], Points[5]));
+        Triangles.push_back(Triangle(Points[3], Points[6], Points[7]));
+        Triangles.push_back(Triangle(Points[3], Points[2], Points[6]));
+        Triangles.push_back(Triangle(Points[4], Points[3], Points[0]));
+        Triangles.push_back(Triangle(Points[4], Points[7], Points[3]));
+        Triangles.push_back(Triangle(Points[5], Points[7], Points[4]));
+        Triangles.push_back(Triangle(Points[5], Points[6], Points[7]));
+
+        ofstream file("Cube.stl");
+        file << "solid" << ' ' << "Cube" << '\n';
+
+        for(int i = 0; i < 12; i++){
+            file << "facet normal" << ' ' << Triangles[i].Normal() << '\n';
+            file << "outer loop" << '\n';
+
+            file << "vertex" << ' ' << Triangles[i].p0() << '\n';
+            file << "vertex" << ' ' << Triangles[i].p1() << '\n';
+            file << "vertex" << ' ' << Triangles[i].p2() << '\n';
+
+            file << "endloop" << '\n';
+            file << "endfacet" << '\n';
+        }
+
+        file << "endsolid" << ' ' << "Cube" << '\n';
     }
 
     double getVolume(){
