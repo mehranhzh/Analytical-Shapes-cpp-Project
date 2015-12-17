5f30f44cf7560192b1d8451a1d02349e5476babc
Mehran <EqSan@users.noreply.github.com>
2015-12-16 20:15:14 -0500

    Update Circle.hh

--- Circle.hh
+++ Circle.hh
@@ -1,23 +1,23 @@
 #ifndef CIRCLE_HH_
 #define CIRCLE_HH_
 
+#include "Shape2D.hh"
 #include "Point.hh"
 
-class Circle : public Point{
+class Circle : public Shape2D{
 private:
     double radius;
+    Point p;
 public:
-    Circle(double x, double y, double r) : Point(x, y, 0) { radius = r; };
-    double getR() const { return radius; };
-    void setR(double r){ radius = r; };
+    Circle(Point p, double r) : Shape2D(p) { radius = r; }
+    double getR() const { return radius; }
+    void setR(double r){ radius = r; }
+    const Point& getCenter() const {return p;}
     double area() const { return PI*radius*radius; }
     void printShapeName() const { cout << "Circle: "; }
     friend ostream& operator <<(ostream& os, const Circle& c){
-        return os << "Center= " << "(" << c.x()<< "," << c.y() << "); Radius= " << c.radius; // Center= (x,y); Radius=
+        return os << "Center= " << "(" << c.getCenter().x()<< "," << c.getCenter().y() << "); Radius= " << c.radius; // Center= (x,y); Radius=
     }
-
-
-
 };
 
 #endif // CIRCLE_H
