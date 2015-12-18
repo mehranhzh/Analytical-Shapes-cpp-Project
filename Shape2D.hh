af10b46320b27d54056cd43e60d5a8dfee03fbf5
Mehran <EqSan@users.noreply.github.com>
2015-12-18 12:22:52 -0500

    Update Shape2D.hh

--- Shape2D.hh
+++ Shape2D.hh
@@ -1,3 +1,4 @@
+//Author:Mehran Hosseinzadeh
 //Shape2D class is derived class of Shape and will show 2D shapes:Circle, Rectangle, Triangle, Square
 #ifndef SHAPE_H_
 #define SHAPE_H_
@@ -6,12 +7,12 @@
 
 class Shape2D : public Shape{
 public:
-    Shape2D(Point p) : Shape(p) {}//Constructor 2D with z_=0
-    double getX(), getY();//get position x,y,z
-    void setX(double x1);//set position x
-    void setY(double y1);//set position y
-    virtual double area() const = 0;//calculate the area of shapes
-    virtual double perimeter() const = 0;//calculate the perimeter of shapes
+    Shape2D() {}//Constructor 2D with z_=0
+    //double getX(), getY();//get position x,y,z
+    //void setX(double x1);//set position x
+    //void setY(double y1);//set position y
+    //virtual double area() const;//calculate the area of shapes
+    //virtual double perimeter() const;//calculate the perimeter of shapes
 
 
 
