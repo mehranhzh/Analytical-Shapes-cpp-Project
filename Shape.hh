7b01fb35582e80410707d94bb558c3c39c095c22
Mehran <EqSan@users.noreply.github.com>
2015-12-18 12:22:17 -0500

    Update Shape.hh

--- Shape.hh
+++ Shape.hh
@@ -1,24 +1,24 @@
+//Author:Mehran Hosseinzadeh
 //Shape class will based class of other shapes
 #ifndef SHAPE_HH_
 #define SHAPE_HH_
 
 #include <iostream>
 #include "Point.hh"
+using namespace std;
 
 
 class Shape {//abstract class shape
-private:
-    Point p;
 public:
-    Shape(const Point& P): p(p){}//Constructor Shape
-    virtual void moveTo(double x1, double y1, double z1)=0;//pure virtual function that move position to new position
-    virtual bool isInside(double x1, double y1, double z1) = 0; //check if point is inside of Shape or not
-    virtual double area() const = 0;//calculate the area of shapes
-    virtual double perimeter() const = 0;//calculate the perimeter of shapes
-    virtual void draw()=0;//draw graphs or shapes
-    virtual void volume()=0;//draw graphs or shapes
-    virtual void printShapeName() const = 0;
-    virtual void print() const = 0;
+    Shape(){}//Constructor Shape
+    //virtual void moveTo(double x1, double y1, double z1);//pure virtual function that move position to new position
+    //virtual bool isInside(double x1, double y1, double z1); //check if point is inside of Shape or not
+    //virtual double area() const;//calculate the area of shapes
+    //virtual double perimeter() const;//calculate the perimeter of shapes
+    //virtual void draw();//draw graphs or shapes
+    //virtual void volume();//draw graphs or shapes
+    //virtual void printShapeName() const;
+    //virtual void print() const;
     virtual ~Shape(){};//Destructor
 
 };
