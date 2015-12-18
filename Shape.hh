93602926155843325f7f5bb316a4da69a7b620af
Mehran <EqSan@users.noreply.github.com>
2015-12-18 13:14:25 -0500

    Update Shape.hh

--- Shape.hh
+++ Shape.hh
@@ -11,14 +11,6 @@ using namespace std;
 class Shape {//abstract class shape
 public:
     Shape(){}//Constructor Shape
-    //virtual void moveTo(double x1, double y1, double z1);//pure virtual function that move position to new position
-    //virtual bool isInside(double x1, double y1, double z1); //check if point is inside of Shape or not
-    //virtual double area() const;//calculate the area of shapes
-    //virtual double perimeter() const;//calculate the perimeter of shapes
-    //virtual void draw();//draw graphs or shapes
-    //virtual void volume();//draw graphs or shapes
-    //virtual void printShapeName() const;
-    //virtual void print() const;
     virtual ~Shape(){};//Destructor
 
 };
